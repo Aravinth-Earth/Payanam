5b9b006760f1bfb4aa5ebf1514f630567a198e85
Aravinth-Earth <55803552+Aravinth-Earth@users.noreply.github.com>
2026-08-19 09:50:47 +0530

    research(kotlin-health): [IMPORT-CLEANUP] remove 2 unused imports from LensesScreen.kt

--- app/src/main/kotlin/io/payanam/ui/screens/LensesScreen.kt
+++ app/src/main/kotlin/io/payanam/ui/screens/LensesScreen.kt
@@ -5,7 +5,6 @@ package io.payanam.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,7 +32,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
