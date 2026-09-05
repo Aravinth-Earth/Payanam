0f1809e2a94301bc0d091d50c190199036d48932
Aravinth-Earth <55803552+Aravinth-Earth@users.noreply.github.com>
2026-09-06 01:47:33 +0530

    Bump version to 1703 - Manual Tests Passed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
           applicationId = "io.payanam"
           minSdk = 28
           targetSdk = 35
-          versionCode = 1701
-          versionName = "1701"
+          versionCode = 1703
+          versionName = "1703"
 
           buildConfigField("boolean", "MINIMAL_MODE", "false")
         buildConfigField("boolean", "SCORING_ENABLED", "true")
